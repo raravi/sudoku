e48786bfb4c0a66a79a9f350dee1b05b94fe37fe
Amith Raravi <amith.raravi@gmail.com>
2020-01-28 19:32:11 +0100

    Initial commit.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,130 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <header className="header">
+        <h1>
+          Su<span className="header__group-one">do</span><span className="header__group-two">ku</span>
+        </h1>
+        <h2>
+          New Game
+        </h2>
       </header>
+      <div className="innercontainer">
+        <section className="game">
+          <table className="game__board">
+            <tbody>
+              <tr className="game__row">
+                <td className="game__cell">8</td>
+                <td className="game__cell"></td>
+                <td className="game__cell"></td>
+                <td className="game__cell"></td>
+                <td className="game__cell">2</td>
+                <td className="game__cell"></td>
+                <td className="game__cell">9</td>
+                <td className="game__cell">1</td>
+                <td className="game__cell"></td>
+              </tr>
+              <tr className="game__row">
+                <td className="game__cell">2</td>
+                <td className="game__cell">3</td>
+                <td className="game__cell">4</td>
+                <td className="game__cell">5</td>
+                <td className="game__cell">1</td>
+                <td className="game__cell"></td>
+                <td className="game__cell"></td>
+                <td className="game__cell"></td>
+                <td className="game__cell">7</td>
+              </tr>
+              <tr className="game__row">
+                <td className="game__cell">7</td>
+                <td className="game__cell">1</td>
+                <td className="game__cell"></td>
+                <td className="game__cell"></td>
+                <td className="game__cell">8</td>
+                <td className="game__cell"></td>
+                <td className="game__cell"></td>
+                <td className="game__cell">5</td>
+                <td className="game__cell">4</td>
+              </tr>
+              <tr className="game__row">
+                <td className="game__cell">6</td>
+                <td className="game__cell"></td>
+                <td className="game__cell"></td>
+                <td className="game__cell">1</td>
+                <td className="game__cell"></td>
+                <td className="game__cell"></td>
+                <td className="game__cell">3</td>
+                <td className="game__cell"></td>
+                <td className="game__cell">5</td>
+              </tr>
+              <tr className="game__row">
+                <td className="game__cell">1</td>
+                <td className="game__cell">8</td>
+                <td className="game__cell">5</td>
+                <td className="game__cell"></td>
+                <td className="game__cell"></td>
+                <td className="game__cell"></td>
+                <td className="game__cell">7</td>
+                <td className="game__cell">2</td>
+                <td className="game__cell"></td>
+              </tr>
+              <tr className="game__row">
+                <td className="game__cell"></td>
+                <td className="game__cell">4</td>
+                <td className="game__cell"></td>
+                <td className="game__cell">6</td>
+                <td className="game__cell"></td>
+                <td className="game__cell">2</td>
+                <td className="game__cell">8</td>
+                <td className="game__cell"></td>
+                <td className="game__cell"></td>
+              </tr>
+              <tr className="game__row">
+                <td className="game__cell"></td>
+                <td className="game__cell">6</td>
+                <td className="game__cell">8</td>
+                <td className="game__cell"></td>
+                <td className="game__cell"></td>
+                <td className="game__cell"></td>
+                <td className="game__cell">4</td>
+                <td className="game__cell"></td>
+                <td className="game__cell"></td>
+              </tr>
+              <tr className="game__row">
+                <td className="game__cell"></td>
+                <td className="game__cell"></td>
+                <td className="game__cell"></td>
+                <td className="game__cell"></td>
+                <td className="game__cell"></td>
+                <td className="game__cell"></td>
+                <td className="game__cell">1</td>
+                <td className="game__cell">6</td>
+                <td className="game__cell">2</td>
+              </tr>
+              <tr className="game__row">
+                <td className="game__cell"></td>
+                <td className="game__cell"></td>
+                <td className="game__cell"></td>
+                <td className="game__cell">4</td>
+                <td className="game__cell"></td>
+                <td className="game__cell">7</td>
+                <td className="game__cell">5</td>
+                <td className="game__cell">3</td>
+                <td className="game__cell"></td>
+              </tr>
+            </tbody>
+          </table>
+        </section>
+        <section className="status">
+        </section>
+
+      </div>
+      <footer className="footer">
+        <p>&#169; 2020 Amith Raravi - source code on <a href="https://github.com/raravi/sudoku">Github</a></p>
+      </footer>
     </div>
   );
 }
