6200006f1d159876c286a1c91b53459d82f1a78d
Ronald van der Louw <r.van.der.louw@gmail.com>
2022-10-14 11:42:22 +0200

    finish module 9

--- cypress/e2e/hint.cy.ts
+++ cypress/e2e/hint.cy.ts
@@ -1,10 +1,12 @@
+/// <reference types="Cypress" />
+
 describe('hint', () => {
   it('fills each empty cell', () => {
     cy.visit('/')
     cy.get('.game__cell.game__cell--filled').should('have.length', 45)
     cy.get('.game__cell')
       .not('.game__cell--filled')
-      .each(($cell) => {
+      .each($cell => {
         cy.wrap($cell).click()
         cy.get('.status__action-hint').click()
       })
