fc381037ea223a4b1e08dadb47474fb7058764ea
Ronald van der Louw <r.van.der.louw@gmail.com>
2022-10-15 15:25:32 +0200

    add scripts and finish module 12

--- cypress/e2e/timer.cy.ts
+++ cypress/e2e/timer.cy.ts
@@ -1,9 +1,19 @@
+/// <reference types="Cypress" />
+
 describe('timer', () => {
-  it('shows 10 seconds',() => {
+  it('shows 10 seconds', () => {
     cy.visit('/')
-    for(let k=0; k < 10 ;k++) {
-      cy.contains('.status__time',`00:0${k}`)
+    for (let k = 0; k < 10; k++) {
+      cy.contains('.status__time', `00:0${k}`)
     }
   })
- })
- 
+  it.only('show minutes and seconds since game started', () => {
+    cy.clock()
+    cy.visit('/')
+    cy.get('.status__time').contains('00:00')
+    cy.tick(30_000)
+    cy.get('.status__time').contains('00:30')
+    cy.tick(30_000)
+    cy.get('.status__time').contains('01:00')
+  })
+})
