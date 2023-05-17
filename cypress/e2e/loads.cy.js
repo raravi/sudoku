6ca9d167da367db6696243d7dfddf73713ddf45c
Felix Svensson <felixsvensson1996@gmail.com>
2023-05-17 16:08:29 +0200

    testing COmmit

--- cypress/e2e/loads.cy.js
+++ cypress/e2e/loads.cy.js
@@ -4,5 +4,6 @@ describe('template spec', () => {
     cy.visit('/')
     cy.contains('.status__time', '00:01')
     cy.get('.game__cell--filled').should('have.length', 45)
+    cy.get('.game__cell--filled').should('have.length', 45)
   })
 })
