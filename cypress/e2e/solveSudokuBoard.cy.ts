a15e9a72642b118be11d98da9d2da010542aa63d
Dan Bonneville <DanMBonneville@gmail.com>
2024-02-26 10:34:59 -0600

    refactor after evaluating better performing solving pattern

--- cypress/e2e/solveSudokuBoard.cy.ts
+++ cypress/e2e/solveSudokuBoard.cy.ts
@@ -1,11 +1,11 @@
 import _ from 'lodash';
 import { useTimer } from 'react-timer-hook';
-import { getStartingSudokuValues, fillMissingValuesInGrid, backtrackSolve, solveBoard } from '../support/utils';
+import { getStartingSudokuValues, fillMissingValuesInGrid, solveBoard } from '../support/utils';
 
 describe('Solve The Sudoku Boards', () => {
   const defaultStabilityOptions = {pollInterval: 1000, timeout: 10000};
   let sudokuBoard: any;
-  let startingIndices: any;
+  let startingCompletedIndices: any;
 
   describe.only('Solve Sudoku Board - Easy', () => {
     const start = performance.now();
@@ -13,16 +13,16 @@ describe('Solve The Sudoku Boards', () => {
       cy.visit('/').then(() => {
         cy.get('#gameBoard').waitForStableDOM(defaultStabilityOptions);
         getStartingSudokuValues().then((startingSudokuValues) => {
-          startingIndices = startingSudokuValues.startingIndices;
+          startingCompletedIndices = startingSudokuValues.startingCompletedIndices;
           sudokuBoard = startingSudokuValues.startingBoard;
-          backtrackSolve(sudokuBoard);
+          solveBoard(sudokuBoard);
         });
       });
     });
   
     it('Solves the puzzle using backtracking', () => {
       console.log("Easy - using elimination", performance.now() - start);
-      fillMissingValuesInGrid(sudokuBoard, startingIndices);
+      fillMissingValuesInGrid(sudokuBoard, startingCompletedIndices);
       cy.contains('solved').should('exist');
     });
   });
@@ -33,60 +33,36 @@ describe('Solve The Sudoku Boards', () => {
         cy.findByTestId('difficulty-select').waitForStableDOM(defaultStabilityOptions).select('Medium');
         cy.get('#gameBoard').waitForStableDOM(defaultStabilityOptions);
         getStartingSudokuValues().then((startingSudokuValues) => {
-          startingIndices = startingSudokuValues.startingIndices;
+          startingCompletedIndices = startingSudokuValues.startingCompletedIndices;
           sudokuBoard = startingSudokuValues.startingBoard;
-          backtrackSolve(sudokuBoard);
+          solveBoard(sudokuBoard);
         });
       });
     });
   
     it('Solves the puzzle using backtracking', () => {
-      fillMissingValuesInGrid(sudokuBoard, startingIndices);
+      fillMissingValuesInGrid(sudokuBoard, startingCompletedIndices);
       cy.contains('solved').should('exist');
     });
   });
 
   describe('Solve Sudoku Board - Hard', () => {
-    const startTime1 = performance.now();
     beforeEach(() => {
       cy.visit('/').then(() => {
         cy.findByTestId('difficulty-select').waitForStableDOM(defaultStabilityOptions).select('Hard');
         cy.get('#gameBoard').waitForStableDOM(defaultStabilityOptions);
         getStartingSudokuValues().then((startingSudokuValues) => {
-          startingIndices = startingSudokuValues.startingIndices;
+          startingCompletedIndices = startingSudokuValues.startingCompletedIndices;
           sudokuBoard = startingSudokuValues.startingBoard;
-          backtrackSolve(sudokuBoard);
+          solveBoard(sudokuBoard);
         });
       });
     });
   
     it('Solves the puzzle using backtracking', () => {
-      const elapsedTime1 = performance.now() - startTime1;
-      console.log("Time to solve using backtracking alone: ", elapsedTime1);
-      fillMissingValuesInGrid(sudokuBoard, startingIndices);
+      fillMissingValuesInGrid(sudokuBoard, startingCompletedIndices);
       cy.contains('solved').should('exist');
     });
   });
-
-  // describe('Solve Sudoku Board - Hard', () => {
-  //   const startTime2 = performance.now();
-  //   beforeEach(() => {
-  //     cy.visit('/').then(() => {
-  //       cy.findByTestId('difficulty-select').waitForStableDOM(defaultStabilityOptions).select('Hard');
-  //       cy.get('#gameBoard').waitForStableDOM(defaultStabilityOptions);
-  //       getStartingSudokuValues().then((startingSudokuValues) => {
-  //         startingIndices = startingSudokuValues.startingIndices;
-  //         sudokuBoard = solveBoard(startingSudokuValues);
-  //       });
-  //     });
-  //   });
-  
-  //   it('Solves the puzzle using elimination THEN backtracking', () => {
-  //     const elapsedTime2 = performance.now() - startTime2;
-  //     console.log("Time to solve using elimination before backtracking: ", elapsedTime2);
-  //     fillMissingValuesInGrid(sudokuBoard, startingIndices);
-  //     cy.contains('solved').should('exist');
-  //   });
-  // });
 
 })
--- cypress/support/utils.ts
+++ cypress/support/utils.ts
@@ -1,49 +1,34 @@
 import _ from 'lodash';
 
 export const getStartingSudokuValues = () => { 
-  type GridMap = { [key: number]: { [key: number]: any } };
+  type GridMap = { [key: number]: { [key: number]: number } };
   let startingBoard: GridMap;
-  let startingIndices: any;
-  startingBoard = new Array(9).fill(null).map(() => new Array(9).fill(null).map(() => new Array(9).fill(true)));
-  startingIndices = [];
+  let startingCompletedIndices: any;
+  startingBoard = new Array(9).fill(null).map(() => new Array(9).fill(null));
+  startingCompletedIndices = [];
   for(let index = 0; index < 81; index++) { 
       let row = getRowFromIndex(index);
       let col = getColFromIndex(index);
       let cellValue;
       cy.findByTestId(`cell_${index}`).then(($element) => {
         cellValue = $element.text();
-        if(cellValue != '0') {
-          startingBoard[row][col] = Number(cellValue);
-          startingIndices.push(index);
-        }
+        startingBoard[row][col] = Number(cellValue);
+        if(cellValue != '0') startingCompletedIndices.push(index);
       });
   };
-  return cy.wrap({startingBoard, startingIndices});
+  return cy.wrap({startingBoard, startingCompletedIndices});
 };
 
-export const solveBoard = (startingSudokuValues: any) => {
-  let solvedIndices = startingSudokuValues.startingIndices;
-  let sudokuGrid= startingSudokuValues.startingBoard
-  while(!isGridComplete(sudokuGrid)) {
-    let prevGridState = _.cloneDeep(sudokuGrid);
-    sudokuGrid = removePossibleValuesForCells(sudokuGrid, solvedIndices);
-    if(_.isEqual(prevGridState, sudokuGrid)){
-      backtrackSolve(sudokuGrid)
-    }
-  }
-  return sudokuGrid;
-}
-
-export const backtrackSolve = (grid: any, r = 0, c = 0): boolean => {
+export const solveBoard = (grid: any, r = 0, c = 0): boolean => {
   if (r == 9) return true;
-  else if (c === 9) return backtrackSolve(grid, r + 1, 0);
-  else if (!Array.isArray(grid[r][c]) && grid[r][c] !== 0) return backtrackSolve(grid, r, c + 1);
+  else if (c === 9) return solveBoard(grid, r + 1, 0);
+  else if (!Array.isArray(grid[r][c]) && grid[r][c] !== 0) return solveBoard(grid, r, c + 1);
   else {
       for (let k = 1; k <= 9; k++) {
           let valuesAssociatedToGrid = getValuesAssociatedToCell(r, c, grid);
           if (!valuesAssociatedToGrid.includes(k)) {
               grid[r][c] = k;
-              if (backtrackSolve(grid, r, c + 1)) {
+              if (solveBoard(grid, r, c + 1)) {
                   return true;
               }
               grid[r][c] = 0;
@@ -71,33 +56,6 @@ export const fillMissingValuesInGrid = (grid: any, alreadySolvedCellIndices: any
   };
 }
 
-// paused for now, while we strategize on how to solve on hard mode
-const isGridComplete = (board: any) => {
-  
-  for(let index =0; index < 81; index++){
-    let row = getRowFromIndex(index);
-    let col = getColFromIndex(index);
-    if(Array.isArray(board[row][col])) return false;
-  }
-  return true;
-}
-
-const removePossibleValuesForCells = (currentBoard: any, solvedIndices: any) => {
-  let newSudokuMap = _.cloneDeep(currentBoard);
-  for(let index = 0; index < 81; index++) {
-    if(solvedIndices.includes(index)) continue;
-    let row = getRowFromIndex(index);
-    let col = getColFromIndex(index);
-    let cellValue = currentBoard[row][col];
-    if(Array.isArray(cellValue)) {
-      let valuesToRemove = getValuesAssociatedToCell(row, col, currentBoard);
-      let newBoolArray = removePossibleValues(valuesToRemove, cellValue);
-      newSudokuMap[row][col] = computeSudokuValue(newBoolArray);
-    }
-  }
-  return newSudokuMap;
-}
-
 const getValuesAssociatedToCell = (row: number, col: number, currentGrid: any) => {
   const valuesToRemoveFromColAndRow = getRowAndColumnValuesAssociatedToCell(row, col, currentGrid);
   const valuesFromLocalBox = getLocalBoxValues(row, col, currentGrid);
@@ -105,61 +63,24 @@ const getValuesAssociatedToCell = (row: number, col: number, currentGrid: any) =
 }
 
 const getRowAndColumnValuesAssociatedToCell = (row: number, col: number, currentGrid: any) => {
-  const valuesToRemove = new Array();
+  const valuesAssociatedToCell = new Array();
   for(let index = 0; index < 9; index++){
-    let valueInColumn = currentGrid[row][index];
-    let valueInRow = currentGrid[index][col];
-
-    if(!Array.isArray(valueInColumn) && !arrayHasValue(valuesToRemove, valueInColumn)) {
-      valuesToRemove.push(valueInColumn);
-    }
-    if(!Array.isArray(valueInRow) && !arrayHasValue(valuesToRemove, valueInRow)) {
-      valuesToRemove.push(valueInRow);
-    }
+    valuesAssociatedToCell.push(currentGrid[row][index]);
+    valuesAssociatedToCell.push(currentGrid[index][col]);
   }
-  return valuesToRemove;
+  return valuesAssociatedToCell;
 }
 
 const getLocalBoxValues = (row: number, col: number, currentGrid: any) => {
   const valuesToRemove: any = [];
   const startingRow = row - (row % 3);
   const startingCol = col - (col % 3);
-
-  const finalRow = startingRow + 3;
-  const finalCol = startingCol + 3;
-
-  for(let rowNum = startingRow; rowNum < finalRow; rowNum++){
-    for(let colNum = startingCol; colNum < finalCol; colNum++) {
-      let valueInCell = currentGrid[rowNum][colNum];
-      if(!Array.isArray(valueInCell) && !arrayHasValue(valuesToRemove, valueInCell)) {
-        valuesToRemove.push(valueInCell);
-      }
+  for(let rowNum = startingRow; rowNum < (startingRow + 3); rowNum++){
+    for(let colNum = startingCol; colNum < (startingCol + 3); colNum++) {
+      valuesToRemove.push(currentGrid[rowNum][colNum]);
     }
   }
   return valuesToRemove;
-}
-
-const arrayHasValue = (array: any, value: any) => {
-  return -1 < Number(array.indexOf(value))
-}
-
-const removePossibleValues = (values: any, booleanArray: any) => {
-  values.map((value: number) => {
-    booleanArray[(Number(value)-1)] = false;
-  });
-  return booleanArray
-}
-
-const computeSudokuValue = (boolArray: any) => {
-  if(arrayHasOneTrueValueLeft(boolArray)) {
-    return (boolArray.indexOf(true) + 1);
-  } else {
-    return boolArray;
-  }
-}
-
-const arrayHasOneTrueValueLeft = (boolArray: any) => {
-  return 1 === boolArray.filter((value: boolean) => value === true).length;
 }
 
 const getRowFromIndex = (index: number) => {
