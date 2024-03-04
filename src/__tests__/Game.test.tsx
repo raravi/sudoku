c6f5c5c3a12a7909a0b8ebbcc44f137d036fc9dc
Dan Bonneville <DanMBonneville@gmail.com>
2024-03-03 19:53:38 -0600

    Adding tests to drive redevelopment of problematic function

---
+++ src/__tests__/Game.test.tsx
@@ -0,0 +1,23 @@
+import { expect, test } from '@jest/globals';
+import { isSolved } from '../utils';
+import {
+  solvedSudokuBoard1,
+  solvedSudokuBoard2,
+  solvedSudokuBoard3,
+  sudokuBoardConflictingValues,
+  sudokuBoardMissingValues,
+} from '../__mocks__/sudokuBoards';
+
+test('Verifys that isSolved returns false with conflicting values on board', () => {
+  expect(isSolved(sudokuBoardConflictingValues)).toBe(false);
+});
+
+test('Verifys that isSolved returns false with missing values on board', () => {
+  expect(isSolved(sudokuBoardMissingValues)).toBe(false);
+});
+
+test('Verifys that isSolved returns true for solved boards', () => {
+  expect(isSolved(solvedSudokuBoard1)).toBe(true);
+  expect(isSolved(solvedSudokuBoard2)).toBe(true);
+  expect(isSolved(solvedSudokuBoard3)).toBe(true);
+});
---
+++ src/utils.tsx
@@ -0,0 +1,4 @@
+// TODO: pass the unit tests TDD
+export function isSolved(gameArray: String[]) {
+  return false;
+}
